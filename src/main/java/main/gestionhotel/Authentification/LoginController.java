54c96cd20151aec8415d3b3bbf505a1980aed572
“[Mohamed <[mohamed5elyousfi@gmail.com]>
2022-01-17 16:19:58 +0100

    Last touch

--- src/main/java/main/gestionhotel/Authentification/LoginController.java
+++ src/main/java/main/gestionhotel/Authentification/LoginController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -51,6 +52,27 @@ public class LoginController {
       Alert alert = new Alert(Alert.AlertType.ERROR);
       alert.setContentText("Not Connected");
       alert.show();
+    }
+  }
+
+  @FXML
+  void check(KeyEvent event) {
+    if (IMetierImpl.connect(this.email.getText(), this.password.getText())) {
+      try{
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("GestionCltEmpMenu.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Menu");
+        stage.setScene(scene);
+        stage.getIcons().add(new Image("https://img.icons8.com/emoji/344/hotel-emoji.png"));
+        stage.show();
+      }catch (Exception e){
+        e.printStackTrace();
+      }
+      Stage stage = (Stage) rootPane.getScene().getWindow();
+      stage.close();
     }
   }
 }
